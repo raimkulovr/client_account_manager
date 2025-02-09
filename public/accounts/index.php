aa2f3c8c16c679a27523883141ad544a0c023a1a
raimkulovr <58400439+raimkulovr@users.noreply.github.com>
2025-02-09 18:03:53 +0500

    add basic pagination for account list view

--- public/accounts/index.php
+++ public/accounts/index.php
@@ -1,7 +1,8 @@
 <?php
 include_once '../../header.php';
-$limit = 100;
+$limit = 10;
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$page = $page < 1 ? 1 : $page;
 $offset = ($page - 1) * $limit;
 
 $stmt = $pdo->prepare(<<<SQL
@@ -13,24 +14,77 @@ LEFT JOIN companies AS comp ON c.company_id = comp.id
 LIMIT :limit OFFSET :offset 
 SQL);
 
-$stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
-$stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
-$stmt->execute();
+$stmt->execute([
+    ':limit' => $limit,
+    ':offset' => $offset,
+]);
 
 $clients = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$currentUrl = BASE_URL . "/accounts/index.php";
+
+$stmt = $pdo->query("SELECT COUNT(*) AS total FROM clients");
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$clients_total = $row['total'];
+$max_page = ceil($clients_total / $limit);
+if ($page > $max_page) {
+    header("Location: " . $currentUrl . "?page=$max_page");
+    exit();
+}
+
+function calculateNumber(int $index)
+{
+    global $page, $limit;
+    return ($page - 1) * $limit + $index + 1;
+}
+
+function renderPageButton(int $page_int)
+{
+    global $page, $currentUrl;
+    echo "<li class=\"page-item\"><a class=\"page-link " . ($page == $page_int ? "active" : "") . "\" href=\"" . $currentUrl . "?page=$page_int" . "\">$page_int</a></li>";
+}
 ?>
 
 <style>
     table td {
-      max-width: 200px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
+        max-width: 200px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
     }
 </style>
 <div class="container flex-grow-1">
     <h1 class="display-6 py-3">Список аккаунтов</h1>
+    <div class="d-flex justify-content-between align-items-center my-3">
+        <p class="text-secondary mb-0">Показ <?php echo calculateNumber(0) ?>-<?php echo calculateNumber(array_key_last($clients) ?? 0) ?> из <?php echo $clients_total ?> аккаунтов</p>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination mb-0">
+                <li class="page-item <?php if ($page == 1) echo "disabled"; ?>">
+                    <a class="page-link" href="<?php echo $currentUrl . "?page=" . $page-1?>" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <?php renderPageButton(1) ?>
+                <?php
+                $range_start = max(2, $page - 3);
+                $range_end = min($max_page - 1, $page + 3);
+
+                for ($i = $range_start; $i <= $range_end; $i++) {
+                    renderPageButton($i);
+                }
+                ?>
+                <?php if ($max_page > 1) renderPageButton($max_page) ?>
+                <li class="page-item <?php if ($page == $max_page) echo "disabled"; ?>">
+                    <a class="page-link" href="<?php echo $currentUrl . "?page=" . $page+1?>" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+
     <table class="table table-striped table-hover">
         <thead class="table-light">
             <tr>
@@ -52,7 +106,7 @@ $clients = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </td>
                 HTML;
 
-            foreach ($clients as $client) {
+            foreach ($clients as $key => $client) {
 
                 $phoneNumbers = array_filter([
                     $client['phone_number1'] ?? null,
@@ -62,12 +116,12 @@ $clients = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 $phoneNumbers = array_map(
                     fn($phoneNumber)
-                        => '<a href="tel:' . $phoneNumber . '">' . $phoneNumber . '</a>',
+                    => '<a href="tel:' . $phoneNumber . '">' . $phoneNumber . '</a>',
                     $phoneNumbers
                 );
 
                 $tr = "<tr>";
-                $tr .= "<td>" . $client['id'] . "</td>";
+                $tr .= "<td>" . calculateNumber($key) . "</td>";
                 $tr .= "<td>" . $client['last_name'] . " " . $client['first_name'] . "</td>";
                 $tr .= "<td>" . "<a href=\"mailto:" . $client['email'] . "\">" . $client['email'] . "</a></td>";
                 $tr .= "<td>" . substr($client['position'], 0, 40) . "</td>";

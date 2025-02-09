5dbbe4d9eb4fbe640050532e9fa875051898cf4e
raimkulovr <58400439+raimkulovr@users.noreply.github.com>
2025-02-09 14:52:43 +0500

    update header -turned tabs into dropdown menus -added BASE_URL constant via config.php -added Bootstrap icons dependency

--- header.php
+++ header.php
@@ -1,36 +1,33 @@
 <?php
-const TABS = [
-	["index.php", "Список"],
-	["add_account.php", "Добавить"],
+const DROPDOWN_TABS = [
+	["accounts/index.php", "Список", [["accounts/index.php", "клиентов"], ["companies/index.php", "компаний"]]],
+	["accounts/add.php", "Добавить", [["accounts/add.php", "клиента"], ["companies/add.php", "компанию"]]],
 ];
 
-require_once 'db-config.php';
+require_once 'config.php';
+require_once 'db_config.php';
 
-function isActivePage($page)
-{
-	return basename($_SERVER['PHP_SELF']) == $page ? 'active' : '';
-}
-
-function renderNavigationTabList(): string
+function renderDropdownNavbarItemList(): string
 {
 	$result = "";
-	foreach (TABS as $tab) {
+
+	foreach (DROPDOWN_TABS as $tab) {
 		$href = $tab[0];
 		$name = $tab[1];
-
-		// open tags
-		$result .= "<li class=\"nav-item\"><a ";
-
-		// apply <a> tag attributes
-		$result .= "href=\"" . $href . "\" ";
-		$result .= "class=\"nav-link " . isActivePage($href) . "\" ";
-		$result .= isActivePage($href) ? "aria-current=\"page\"" : "";
-
-		// add <a> tag text
-		$result .= ">$name";
-
-		// close tags
-		$result .= "</a></li>";
+		$menu_items = $tab[2];
+		
+		$result .= "<li class=\"nav-item dropdown\"><a role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\" class=\"nav-link dropdown-toggle\"";
+		$result .= "href=\"" . $href . "\">$name</a>";
+		$result .= "<ul class=\"dropdown-menu\">";
+			foreach($menu_items as $item) {
+				$item_href = $item[0];
+				$item_name = $item[1];
+				$result .= "<li><a ";
+				$result .= "href=\"" . $item_href . "\" ";
+				$result .= "class=\"dropdown-item\"";
+				$result .= ">$item_name</a></li>";
+			}
+		$result .= "</ul></li>";
 	}
 	return $result;
 }
@@ -43,10 +40,11 @@ function renderNavigationTabList(): string
 	<meta charset="utf-8">
 	<meta name="description" content="A short description." />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<base href="<?php echo BASE_URL ?>">
 	<title>Client Account Manager</title>
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<style>
 		.navbar-brand {
 			padding-top: 0;
@@ -54,7 +52,7 @@ function renderNavigationTabList(): string
 	</style>
 </head>
 
-<body>
+<body class="d-flex flex-column min-vh-100">
 	<header>
 		<nav class="navbar navbar-expand-lg bg-body-tertiary">
 			<div class="container">
@@ -64,7 +62,7 @@ function renderNavigationTabList(): string
 				</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-						<?php echo renderNavigationTabList(); ?>
+						<?php echo renderDropdownNavbarItemList(); ?>
 					</ul>
 					<form class="d-flex mb-2 mb-lg-0" role="search">
 						<input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Поиск">

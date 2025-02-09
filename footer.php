059da079b14e2f2189351a77fe683bfa7ab7e4ec
raimkulovr <58400439+raimkulovr@users.noreply.github.com>
2025-02-09 14:44:15 +0500

    add Popper via bootstrap

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="container mt-auto">
 	<div class="pt-5">
-	<p class="text-center">&copy; <?php echo date("Y"); ?> R. Radmir. All rights reserved.</p>
+		<p class="text-center">&copy; <?php echo date("Y"); ?> R. Radmir. All rights reserved.</p>
 	</div>
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
